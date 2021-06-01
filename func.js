ef6db636ea36ba2713c73e4e3e1262b53bf434b3
asutoss <ashutoshpal0456@gmail.com>
2021-06-01 23:14:31 +0530

    website updates

--- func.js
+++ func.js
@@ -27,3 +27,15 @@ function scrollToContact (e) {
         behavior:"smooth"
     });
 }
+
+
+function toggleVis (e) {
+    e.preventDefault();
+    var x = document.getElementById('extraWork');
+    
+    if (x.style.display === "none" || !x.style.display) {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
